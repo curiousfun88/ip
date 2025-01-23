1ca8160d5f8ef7057d7706b5bec9bb9d9662b81a
curiousfun88 <curiouslearner842@gmail.com>
2025-01-24 01:18:59 +0800

    Level-2: Add, List

--- src/main/java/Blob.java
+++ src/main/java/Blob.java
@@ -9,6 +9,8 @@ public class Blob {
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
 
+        ArrayList<String> list = new ArrayList<>();
+
         while (true) {
 
             String command = scanner.nextLine();
@@ -18,11 +20,20 @@ public class Blob {
                 System.out.println("    Bye. Hope to see you again soon!");
                 System.out.println("    ____________________________________________________________");
                 break;
+            } else if (command.equals("list")) {
+                System.out.println("    ____________________________________________________________");
+                for (int i = 0; i < list.size(); i++) {
+                    int index = i + 1;
+                    String content = list.get(i);
+                    System.out.println("    " + index + ". " + content);
+                }
+                System.out.println("    ____________________________________________________________");
+            } else {
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    added: " + command);
+                System.out.println("    ____________________________________________________________");
+                list.add(command);
             }
-
-            System.out.println("    ____________________________________________________________");
-            System.out.println("    " + command);
-            System.out.println("    ____________________________________________________________");
         }
         scanner.close();
     }

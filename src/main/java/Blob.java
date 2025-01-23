37bd00e85fa8359465891f084032520dd57574fc
curiousfun88 <curiouslearner842@gmail.com>
2025-01-24 00:52:41 +0800

    Level-1: Echo

--- src/main/java/Blob.java
+++ src/main/java/Blob.java
@@ -1,11 +1,29 @@
+import java.util.*;
+
 public class Blob {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Blob");
+        System.out.println(" Hello! I'm Blob!!");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
 
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        while (true) {
+
+            String command = scanner.nextLine();
+
+            if (command.equals("bye")) {
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    Bye. Hope to see you again soon!");
+                System.out.println("    ____________________________________________________________");
+                break;
+            }
+
+            System.out.println("    ____________________________________________________________");
+            System.out.println("    " + command);
+            System.out.println("    ____________________________________________________________");
+        }
+        scanner.close();
     }
 }

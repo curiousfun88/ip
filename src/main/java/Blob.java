6f0e08555ba42994176db3031f38654b32da6b5e
curiousfun88 <curiouslearner842@gmail.com>
2025-01-24 02:06:25 +0800

    Level-3: Mark as Done

--- src/main/java/Blob.java
+++ src/main/java/Blob.java
@@ -9,7 +9,7 @@ public class Blob {
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
 
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
 
         while (true) {
 
@@ -22,19 +22,63 @@ public class Blob {
                 break;
             } else if (command.equals("list")) {
                 System.out.println("    ____________________________________________________________");
-                for (int i = 0; i < list.size(); i++) {
+                System.out.println("    Here are the tasks in your list:");
+                for (int i = 0; i < tasks.size(); i++) {
                     int index = i + 1;
-                    String content = list.get(i);
+                    Task content = tasks.get(i);
                     System.out.println("    " + index + ". " + content);
                 }
+                System.out.println("    ____________________________________________________________");
+            } else if (command.startsWith("mark")) {
+                int index = Integer.parseInt(command.split(" ")[1]);
+                Task selected = tasks.get(index - 1);
+                selected.markAsDone();
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    Nice! I've marked this task as done:");
+                System.out.println("        " + selected);
+                System.out.println("    ____________________________________________________________");
+            } else if (command.startsWith("unmark")) {
+                int index = Integer.parseInt(command.split(" ")[1]);
+                Task selected = tasks.get(index - 1);
+                selected.markAsNotDone();
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    OK, I've marked this task as not done yet:");
+                System.out.println("        " + selected);
                 System.out.println("    ____________________________________________________________");
             } else {
                 System.out.println("    ____________________________________________________________");
                 System.out.println("    added: " + command);
                 System.out.println("    ____________________________________________________________");
-                list.add(command);
+                tasks.add(new Task(command));
             }
         }
         scanner.close();
+    }
+}
+
+class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
     }
 }
